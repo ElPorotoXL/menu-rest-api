7f8e1ce2956f44ac1d3c4e91b8f1a510bb84eaf6
ElPorotoXL <guillesilvero830@gmail.com>
2026-07-07 15:48:51 -0300

    migrate into mysql2

--- db/db.js
+++ db/db.js
@@ -1,27 +1,58 @@
-import mariadb from "mariadb";
+import mysql from "mysql2/promise";
 
-const pool = mariadb.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 5,
-    ssl: {
-        rejectUnauthorized: false
-    },
-    connectTimeout: 60000
+    connectionLimit: 5
 });
 
 export async function query(sql, params) {
-    let conn;
     try {
-        conn = await pool.getConnection();
-        const result = await conn.query(sql, params);
-        return result;
+        const [rows] = await pool.execute(sql, params);
+        return rows;
     } catch (error) {
         console.error(error);
         throw error;
-    } finally {
-        if (conn) conn.release();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// import mariadb from "mariadb";
+// 
+// const pool = mariadb.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     connectionLimit: 5,
+//     ssl: {
+//         rejectUnauthorized: false
+//     },
+//     connectTimeout: 60000
+// });
+// 
+// export async function query(sql, params) {
+//     let conn;
+//     try {
+//         conn = await pool.getConnection();
+//         const result = await conn.query(sql, params);
+//         return result;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+// }
