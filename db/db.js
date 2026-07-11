45b00227a6d9842834bf536720079aa9e1fbb692
ElPorotoXL <guillesilvero830@gmail.com>
2026-07-11 09:38:20 -0300

    feat: use render postgresql database

--- db/db.js
+++ db/db.js
@@ -1,32 +1,27 @@
-import mysql from "mysql2/promise";
+import pg from "pg";
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    connectionLimit: 5,
-    waitForConnections: true,
-    queueLimit: 0,
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     },
-    connectTimeout: 60000
+    max: 10,
+    idleTimeoutMillis: 30000
 });
 
 export async function query(sql, params) {
     try {
-        let conn;
-        conn = await pool.getConnection();
-        const [rows] = await conn.execute(sql, params);
+        const { rows } = await pool.query(sql, params);
         return rows;
     } catch (error) {
         console.error(error);
         throw error;
-    } finally {
-        if (conn) conn.release();
     }
 }
+
+
+
+
 
 
 
@@ -59,6 +54,58 @@ export async function query(sql, params) {
 //         conn = await pool.getConnection();
 //         const result = await conn.query(sql, params);
 //         return result;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     } finally {
+//         if (conn) conn.release();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mysql from "mysql2/promise";
+// 
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     connectionLimit: 5,
+//     waitForConnections: true,
+//     queueLimit: 0,
+//     ssl: {
+//         rejectUnauthorized: false
+//     },
+//     connectTimeout: 60000
+// });
+// 
+// export async function query(sql, params) {
+//     try {
+//         let conn;
+//         conn = await pool.getConnection();
+//         const [rows] = await conn.execute(sql, params);
+//         return rows;
 //     } catch (error) {
 //         console.error(error);
 //         throw error;
