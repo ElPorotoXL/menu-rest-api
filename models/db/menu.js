ce5797b97cff0f4b653716c24a6d34a9f44ac190
ElPorotoXL <guillesilvero830@gmail.com>
2026-07-11 09:49:24 -0300

    fix: postgre causing case-sensitive into the queries...

--- models/db/menu.js
+++ models/db/menu.js
@@ -2,27 +2,27 @@ import { query } from "../../db/db.js"
 
 export class MenuModel {
     static async getAll () {
-        const sql = 'SELECT * FROM Menu'
+        const sql = 'SELECT * FROM menu'
         const result = await query(sql)
         return result
     }
     static async getId ({ id }) {
-        const sql = 'SELECT * FROM Menu WHERE IdMenu = ?'
+        const sql = 'SELECT * FROM menu WHERE idmenu = ?'
         const result = await query(sql, [id])
         return result
     }
     static async create ({ data }) {
-        const sql = 'INSERT INTO Menu (Name, Price, Description, Rate) VALUES (?, ?, ?, ?)'
+        const sql = 'INSERT INTO menu (name, price, description, rate) VALUES (?, ?, ?, ?)'
         const result = await query(sql, [data.name, data.price, data.description, data.rate])
         return result
     }
     static async update ({ id, data }) {
-        const sql = 'UPDATE Menu SET Name = ?, Price = ?, Description = ?, Rate = ? WHERE IdMenu = ?'
+        const sql = 'UPDATE menu SET name = ?, price = ?, description = ?, rate = ? WHERE idmenu = ?'
         const result = await query(sql, [data.name, data.price, data.description, data.rate, id])
         return result
     }
     static async delete ({ id }) {
-        const sql = 'DELETE FROM Menu WHERE IdMenu = ?'
+        const sql = 'DELETE FROM menu WHERE idmenu = ?'
         const result = await query(sql, [id])
         return result
     }
