5b9fb2baa2798a1ee14446e231e32ec7d3bfb48c
ElPorotoXL <guillesilvero830@gmail.com>
2026-07-11 10:15:36 -0300

    fix: even more case-sensitive problems

--- models/db/menu.js
+++ models/db/menu.js
@@ -7,22 +7,22 @@ export class MenuModel {
         return result
     }
     static async getId ({ id }) {
-        const sql = 'SELECT * FROM menu WHERE idmenu = ?'
+        const sql = 'SELECT * FROM menu WHERE idmenu = $1'
         const result = await query(sql, [id])
         return result
     }
     static async create ({ data }) {
-        const sql = 'INSERT INTO menu (name, price, description, rate) VALUES (?, ?, ?, ?)'
-        const result = await query(sql, [data.name, data.price, data.description, data.rate])
+        const sql = 'INSERT INTO menu (name, price, description, rate) VALUES ($1, $2, $3, $4)'
+        const result = await query(sql, [data.name, data.price , data.description, data.rate])
         return result
     }
     static async update ({ id, data }) {
-        const sql = 'UPDATE menu SET name = ?, price = ?, description = ?, rate = ? WHERE idmenu = ?'
+        const sql = 'UPDATE menu SET name = $1, price = $2, description = $3, rate = $4 WHERE idmenu = $5'
         const result = await query(sql, [data.name, data.price, data.description, data.rate, id])
         return result
     }
     static async delete ({ id }) {
-        const sql = 'DELETE FROM menu WHERE idmenu = ?'
+        const sql = 'DELETE FROM menu WHERE idmenu = $1'
         const result = await query(sql, [id])
         return result
     }
